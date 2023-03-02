f8edf9785c1c50430b00b08413b81b61303a3f66
Marius Pop <33959561+mariuspop86@users.noreply.github.com>
2023-03-02 11:04:24 +0200

    Load users from file. Added a user provider. Added email in the JWT token.

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Security\User;
+use App\Security\UserProvider;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    private $userProvider;
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct(UserProvider $userProvider)
+    {
+        $this->userProvider = $userProvider;
+    }
+
+    /**
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload = $event->getData();
+        /** @var $user User */
+        $user = $this->userProvider->loadUserByIdentifier($payload['username']);
+        $payload['email'] = $user->getEmail();
+        $event->setData($payload);
+    }
+}
