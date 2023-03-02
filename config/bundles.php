2e617d2411993904ee0da2f1b4ffb82c9a949d0c
Marius Pop <33959561+mariuspop86@users.noreply.github.com>
2023-03-02 11:13:50 +0200

    Merge branch 'main' into saml_sso
    # Conflicts:
    #	composer.lock
    #	config/bundles.php
    #	config/packages/security.yaml

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Hslavich\OneloginSamlBundle\HslavichOneloginSamlBundle::class => ['all' => true],
 ];
---
+++ config/users.php
@@ -0,0 +1,9 @@
+<?php
+$test = [
+    'username' => 'test', 
+    'pwd' => '$2y$13$926fEY/N4ljYYCOuvGUWouppQny9dfDboutMzM0ys7h.jZWZCXrQ6', 
+    'email' => 'test@test.com',
+    'roles' => [
+        'ROLE_ADMIN'
+    ]
+];
